2a9614cd6eec24b6801fee21d973560dac0fce71
William Combee <william.combee@yahoo.com>
2023-06-07 10:25:46 -0500

    added to webpack

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,33 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
-
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
-
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent the default behavior of the event
+    event.preventDefault();
+    // Save the event to a variable for later use
+    // This event contains the necessary information to prompt the user to install the app
+    const installPromptEvent = event;
+    // Show the installation button or perform other actions
+  });
+  
+  butInstall.addEventListener('click', async () => {
+    // Ensure the `beforeinstallprompt` event was previously captured and stored
+    if (installPromptEvent) {
+      // Show the installation prompt to the user
+      installPromptEvent.prompt();
+      // Wait for the user to respond to the prompt
+      const choiceResult = await installPromptEvent.userChoice;
+      // Process the user's choice
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the installation prompt.');
+      } else {
+        console.log('User dismissed the installation prompt.');
+      }
+      // Clear the stored event after it has been used
+      installPromptEvent = null;
+    }
+  });
+  
+  window.addEventListener('appinstalled', (event) => {
+    console.log('App installed successfully!');
+  });
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,47 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/install.html',
+        filename: 'install.html',
+        chunks: ['install'],
+      }),
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '225ca3',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'sw.js',
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
       ],
     },
   };
